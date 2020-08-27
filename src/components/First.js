0634a66a24f19fbb5d6ddadd914a58765ae28184
Marshall Johnson <marshalljohnson193@gmail.com>
2020-08-27 07:50:15 -0600

    almost done

---
+++ src/components/First.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './first.css'
+
+export default function First() {
+    return (
+        <div>
+            <div className='greeting'>
+                <p className='welcome'>welcome to our studio!</p>
+                <p className='greet'>it's nice to meet you</p>
+                <button className='tell-more'>tell me more</button>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import './navbar.css'
+
+export default class NavBar extends Component {
+    constructor() {
+        super()
+        this.state = {
+            display: false
+        }
+    }
+
+    handleDisplay() {
+        this.setState({ display: !this.state.display })
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <header className='header'>
+                    <nav className='navbar'>
+                        <h2 className='app-name'><a><img src='https://startbootstrap.github.io/startbootstrap-agency/assets/img/navbar-logo.svg' /></a></h2>
+
+                        <div className='navbar-icon' onClick={() => this.handleDisplay()}>MENU &#9776; </div>
+
+                        <div className='button-container'>
+                            <button className='navbar-button'>Services</button>
+                            <button className='navbar-button'>portfolio</button>
+                            <button className='navbar-button'>about</button>
+                            <button className='navbar-button'>team</button>
+                            <button className='navbar-button'>contact</button>
+                        </div>
+
+                        <div className={this.state.display ? 'dropdown' : 'hidden'}>
+                            <button className='navbar-button'>Services</button>
+                            <button className='navbar-button'>portfolio</button>
+                            <button className='navbar-button'>about</button>
+                            <button className='navbar-button'>team</button>
+                            <button className='navbar-button'>contact</button>
+                        </div>
+
+                    </nav>
+                </header>
+            </div>
+        )
+    }
+}
